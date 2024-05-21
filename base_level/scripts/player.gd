3c4aef1a7de42dc8d26068973b4060f3f802575a
biarog <beatrizrogers55@gmail.com>
2024-05-21 17:51:34 -0300

    Alteracoes feitas em cenas ja existentes
    Tilemap modificado de tal forma a nao precisar da cena plataforma, colisao do jogador modificada, delecao de plataforma e seu script

--- base_level/scripts/player.gd
+++ base_level/scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -625.0
+const JUMP_VELOCITY = -615.0
+const SKATE_VELOCITY = 250.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	velocity.x = SKATE_VELOCITY
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
@@ -15,4 +17,4 @@ func _physics_process(delta):
 
 func _input(event : InputEvent):
 	if event.is_action_pressed("move_down") && is_on_floor() :
-		position.y += 1 
+		position.y += 5
