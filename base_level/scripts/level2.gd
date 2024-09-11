42316ac93741888364e0fcd5756f9604daf81779
biarog <66644264+biarog@users.noreply.github.com>
2024-09-11 18:08:52 -0300

    Conclusao d nivel 2
    Foi feita a conclusão do nivel 2

--- base_level/scripts/level2.gd
+++ base_level/scripts/level2.gd
@@ -5,9 +5,18 @@ var boss_vel = 430
 var subir:bool = false
 
 func _ready():
-	boss.get_node("AnimacaoFox").play("idle")
+	lvl_number = get_tree().get_current_scene().get_name().trim_prefix("Level ").to_int() - 1
 	node_progress_bar.set("min_value", get_node("InicioLVL").position.x)
 	node_progress_bar.set("max_value", get_node("FinalLVL").position.x)
+	
+	for i in 3:
+		mixtapes[i] = playerData.lvl_Mixtapes[lvl_number][i+1]
+		var node_mix = get_node("Colecionaveis/Mixtape" + str(i+1))
+		node_mix.mix_number = i
+		node_mix.mix_status = mixtapes[i]
+		node_mix.update_frame()
+	
+	boss.get_node("AnimacaoFox").play("idle")
 
 func _process(delta):
 	
@@ -15,5 +24,9 @@ func _process(delta):
 	if !win_condition and boss.position.x >= (node_cam.position.x - 465):
 		boss_vel = 412
 		node_cam.position.x += 12 * delta
+		if node_cam.position.x >= 9915 and node_cam.position.y >= -327:
+			node_cam.position.y -= 100 * delta
+	
+	
 	
 	node_progress_bar.set("value", node_player.position.x)
